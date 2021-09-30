c3c7b005291680801e169c2717d9b1f61ee00193
davidbrylka <gt-turbo@jagware.org>
2021-09-30 16:29:28 +0200

    30/09/2021 16:29

--- assets/php/src/NewSet.php
+++ assets/php/src/NewSet.php
@@ -4,20 +4,31 @@ namespace Pingpong\Dada;
 
 class NewSet
 {
-    private array $Score = array(0);
+    private array $Score = array();
     private int $delta;
+
 
     function playSet(): int
     {
-        do {
+        $this->Score[0] = 0;
+        $this->Score[1] = 0;
+
+        // do {
+        for ($i = 0; $i < 10; $i++) {
             $random = rand(0, 1);
+            echo $random;
 
-            $this->Score[$random] = 5;
+            var_dump($this->Score[0]);
+            var_dump($this->Score[1]);
+            $this->Score[$random]++;
+            var_dump($this->Score[0]);
+            var_dump($this->Score[1]);
 
-            $this->delta = abs($this->Score[0] - $this->Score[1]);
-        } while (($this->Score[0] or $this->Score[1]) and $this->delta > 1);
-
-        return 0;
+            // $this->delta = abs($this->Score[0] - $this->Score[1]);
+            // } while (($this->Score[0] or $this->Score[1]) and $this->delta > 1);
+            // } while (true);
+        }
+        return $random;
         echo ('Fin de set');
     }
 }
