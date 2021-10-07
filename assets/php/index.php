0421174c2481aa2cf0e68a71e14a8d3bb56e2d8f
davidbrylka <gt-turbo@jagware.org>
2021-10-07 09:10:15 +0200

    07/10/2021 09:10

--- assets/php/index.php
+++ assets/php/index.php
@@ -18,11 +18,8 @@ $match->StartNewMatch();
 
 $currentSet = $match->getCurrentSet();
 
-$actualSetWon = 0;
-
-// for ($i = 0; $i < 400; $i++) {
-// $lastSet=$currentSet;
 do {
+  
     if (!$currentSet->isFinished()) {
         $currentSet->addPoint(rand(0, 1));
     } else {                        // If set finished, verify 3 winning sets
@@ -30,11 +27,7 @@ do {
         $winner = $currentSet->giveWinner();
         echo ("set gagné par : " . $Players[$currentSet->giveWinner()]->getName() . "<br> ");
         $setwon[$currentSet->giveWinner()]++;
-        $actualSetWon = $setwon[$currentSet->giveWinner()]>=3;
-        
-        if ($setwon[$currentSet->giveWinner()] >= 3) {
-            break;
-        }
+        $actualSetWon = $setwon[$currentSet->giveWinner()] >= 3;
 
         if ($setwon[$currentSet->giveWinner()] < 3) {
             echo ($setwon[$currentSet->giveWinner()] . " <br>");
@@ -42,7 +35,9 @@ do {
             $currentSet = $match->getCurrentSet();
         }
     }
-} while (true);               // ($actualSetWon);
+} while ($setwon[$currentSet->giveWinner()]<3);
+
+Echo ("Match gagné par : ".$Players[$currentSet->giveWinner()]->getName());
 
 
-die();
+
